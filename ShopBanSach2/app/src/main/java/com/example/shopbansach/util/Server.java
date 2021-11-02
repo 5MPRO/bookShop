dde85179fa2e6b329f2a4d742161896610bd61fe
vanthienpham <vanthien.dev@gmail.com>
2021-11-02 22:50:14 +0700

    thien up again

---
+++ ShopBanSach2/app/src/main/java/com/example/shopbansach/util/Server.java
@@ -0,0 +1,13 @@
+package com.example.shopbansach.util;
+
+public class Server {
+    public static String localhost="192.168.43.60";
+    public static String Duongdanloaisp = "http://" + localhost + "/BanSach/getloaisanpham.php";
+    public static String Duongdansanphammoinhat = "http://" + localhost + "/BanSach/getsanphammoinhat.php";
+    public static String Duongdansanphamgiamgia = "http://" + localhost + "/BanSach/getsanphamgiamgia.php";
+    public static String Duongdantruyentranh = "http://" + localhost + "/BanSach/getsanpham.php?page=";
+    public static String Đuongdandonhang = "http://"+localhost+"/BanSach/thongTinKhachHang.php";
+    public static String Đuongdanchitietdonhang = "http://"+localhost+"/BanSach/chiTietDonHang.php";
+    public static String Đuongdandangnhap = "http://"+localhost+"/LoginBook/login.php";
+    public static String Đuongdandangky = "http://"+localhost+"/LoginBook/register.php";
+}
