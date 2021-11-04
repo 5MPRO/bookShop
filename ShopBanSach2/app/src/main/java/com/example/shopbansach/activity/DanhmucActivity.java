011d6a3620cddf0362e79e5b1752202da0ccd2cf
huykt1111 <huylmht10@gmail.com>
2021-11-04 08:34:55 +0700

    HUY

--- ShopBanSach2/app/src/main/java/com/example/shopbansach/activity/DanhmucActivity.java
+++ ShopBanSach2/app/src/main/java/com/example/shopbansach/activity/DanhmucActivity.java
@@ -6,28 +6,111 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.shopbansach.R;
+import com.example.shopbansach.adapter.AllSanphamAdapter;
+import com.example.shopbansach.model.Sanpham;
+import com.example.shopbansach.util.CheckConnection;
+import com.example.shopbansach.util.Server;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DanhmucActivity extends AppCompatActivity {
     Toolbar toolbardm;
+    int idsp = 0;
     LinearLayout ln_home,ln_tk,ln_tb,ln_search,ln_dm;
+    ListView lvsp;
+    AllSanphamAdapter sanphamAdapter;
+    ArrayList<Sanpham> mangsp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_danhmuc);
         AnhXa();
         OnClickMenu();
-        ActionToolbar();
+        if (CheckConnection.haveNetworkConnection(getApplicationContext())){
+            ActionToolbar();
+            GetData();
+        }else {
+            CheckConnection.ShowToast_Short(getApplicationContext(),"Hãy kiểm tra lại kết nối Internet");
+            finish();
+        }
+
+    }
+
+    private void GetData() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        String duongdan = Server.Duongdanallsp;
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, duongdan, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                int id = 0;
+                String Tensp = "";
+                int Giasp;
+                String Hinhanhsp="";
+                String Motasp="";
+                int Idsp=0;
+                if(response!=null){
+                    try {
+                        JSONArray jsonArray = new JSONArray(response);
+                        for (int i = 0;i<jsonArray.length();i++){
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            id = jsonObject.getInt("id");
+                            Tensp = jsonObject.getString("tensp");
+                            Giasp = jsonObject.getInt("giasp");
+                            Hinhanhsp = jsonObject.getString("hinhanhsp");
+                            Motasp = jsonObject.getString("motasp");
+                            Idsp = jsonObject.getInt("idsanpham");
+                            mangsp.add(new Sanpham(id,Tensp,Giasp,Hinhanhsp,Motasp,Idsp));
+                            sanphamAdapter.notifyDataSetChanged();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> parram = new HashMap<String,String>();
+                parram.put("idSanPham",String.valueOf(idsp));
+                return super.getParams();
+            }
+        };
+        requestQueue.add(stringRequest);
     }
 
     private void AnhXa() {
             toolbardm = findViewById(R.id.toolbardanhmuc);
+            lvsp = findViewById(R.id.listviewdanhmuc);
+            mangsp = new ArrayList<>();
+            sanphamAdapter = new AllSanphamAdapter(getApplicationContext(),mangsp);
+            lvsp.setAdapter(sanphamAdapter);
             ln_home = findViewById(R.id.ln_home);
             ln_tk = findViewById(R.id.ln_tk);
             ln_tb = findViewById(R.id.ln_tb);
             ln_dm = findViewById(R.id.ln_dm);
             ln_search = findViewById(R.id.ln_search);
+
         }
 
     private void OnClickMenu() {
--- ShopBanSach2/app/src/main/java/com/example/shopbansach/util/Server.java
+++ ShopBanSach2/app/src/main/java/com/example/shopbansach/util/Server.java
@@ -3,6 +3,7 @@ package com.example.shopbansach.util;
 public class Server {
     public static String localhost="192.168.1.109";
     public static String Duongdanloaisp = "http://" + localhost + "/BanSach/getloaisanpham.php";
+    public static String Duongdanallsp = "http://" + localhost + "/BanSach/getallsanpham.php";
     public static String Duongdansanphammoinhat = "http://" + localhost + "/BanSach/getsanphammoinhat.php";
     public static String Duongdansanphamgiamgia = "http://" + localhost + "/BanSach/getsanphamgiamgia.php";
     public static String Duongdantruyentranh = "http://" + localhost + "/BanSach/getsanpham.php?page=";
