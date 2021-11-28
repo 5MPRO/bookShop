161b3bb8fb9f8d8606702c3f1bed671867ef8b01
sybuivan <sybuivan1429@gmail.com>
2021-11-28 16:25:07 +0700

    add delete

--- ShopBanSach2/app/src/main/java/com/example/shopbansach/util/Server.java
+++ ShopBanSach2/app/src/main/java/com/example/shopbansach/util/Server.java
@@ -2,7 +2,6 @@ package com.example.shopbansach.util;
 
 public class Server
 {
-<<<<<<< HEAD
 public static String localhost="192.168.1.6";
 public static String Duongdanloaisp = "http://" + localhost + "/BanSach/getloaisanpham.php";
 public static String Duongdanallsp = "http://" + localhost + "/BanSach/getallsanpham.php";
@@ -12,17 +11,6 @@ public static String Duongdantruyentranh = "http://" + localhost + "/BanSach/get
 public static String Đuongdandonhang = "http://"+localhost+"/BanSach/thongTinKhachHang.php";
 public static String Đuongdanchitietdonhang = "http://"+localhost+"/BanSach/chiTietDonHang.php";
 public static String Đuongdandangnhap = "http://"+localhost+"/LoginBook/login.php";
-public static String Đuongdandangky = "http://"+localhost+"/LoginBook/register.php";}
-=======
-        public static String localhost="192.168.1.109";
-        public static String Duongdanloaisp = "http://" + localhost + "/BanSach/getloaisanpham.php";
-        public static String Duongdanallsp = "http://" + localhost + "/BanSach/getallsanpham.php";
-        public static String Duongdansanphammoinhat = "http://" + localhost + "/BanSach/getsanphammoinhat.php";
-        public static String Duongdansanphamgiamgia = "http://" + localhost + "/BanSach/getsanphamgiamgia.php";
-        public static String Duongdantruyentranh = "http://" + localhost + "/BanSach/getsanpham.php?page=";
-        public static String Đuongdandonhang = "http://"+localhost+"/BanSach/thongTinKhachHang.php";
-        public static String Đuongdanchitietdonhang = "http://"+localhost+"/BanSach/chiTietDonHang.php";
-        public static String Đuongdandangnhap = "http://"+localhost+"/LoginBook/login.php";
-        public static String Đuongdandangky = "http://"+localhost+"/LoginBook/register.php";
+public static String Đuongdandangky = "http://"+localhost+"/LoginBook/register.php";
+
 }
->>>>>>> 3933e6602b97c2e9b2530c70467b2a6e54acce85
