20fa8f0c4bb8672d32cbdb63ec129b882044c77d
sybuivan <sybuivan1429@gmail.com>
2021-11-28 16:13:39 +0700

    add

--- ShopBanSach2/app/src/main/java/com/example/shopbansach/util/Server.java
+++ ShopBanSach2/app/src/main/java/com/example/shopbansach/util/Server.java
@@ -2,7 +2,7 @@ package com.example.shopbansach.util;
 
 public class Server
 {
-public static String localhost="192.168.43.60";
+public static String localhost="192.168.1.6";
 public static String Duongdanloaisp = "http://" + localhost + "/BanSach/getloaisanpham.php";
 public static String Duongdanallsp = "http://" + localhost + "/BanSach/getallsanpham.php";
 public static String Duongdansanphammoinhat = "http://" + localhost + "/BanSach/getsanphammoinhat.php";
@@ -11,5 +11,4 @@ public static String Duongdantruyentranh = "http://" + localhost + "/BanSach/get
 public static String Đuongdandonhang = "http://"+localhost+"/BanSach/thongTinKhachHang.php";
 public static String Đuongdanchitietdonhang = "http://"+localhost+"/BanSach/chiTietDonHang.php";
 public static String Đuongdandangnhap = "http://"+localhost+"/LoginBook/login.php";
-public static String Đuongdandangky = "http://"+localhost+"/LoginBook/register.php";
-        }
+public static String Đuongdandangky = "http://"+localhost+"/LoginBook/register.php";}
