3f3b2573f8910b88a76602072fa6973e8450ff76
huykt1111 <huylmht10@gmail.com>
2021-12-04 20:16:21 +0700

    huy cart

--- ShopBanSach2/app/src/main/java/com/example/shopbansach/activity/Thongtinkhachhang.java
+++ ShopBanSach2/app/src/main/java/com/example/shopbansach/activity/Thongtinkhachhang.java
@@ -8,15 +8,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.shopbansach.R;
+import com.example.shopbansach.model.emailLogin;
 import com.example.shopbansach.util.CheckConnection;
 import com.example.shopbansach.util.Server;
 
@@ -28,15 +31,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Thongtinkhachhang extends AppCompatActivity {
-    EditText edttenkhachhang,edtemail,edtsdt;
+    EditText edttenkhachhang,edtemail,edtsdt,edtaddress;
     Button btnxacnhan,btntrove;
     String email;
+    int idUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thongtinkhachhang);
         Anhxa();
         getEmail();
+        readJson(Server.Duongdaninfor +"?email="+ emailLogin.email.trim());
         btntrove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,11 +62,42 @@ public class Thongtinkhachhang extends AppCompatActivity {
 
         }
     }
+    private void readJson(String url){
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url,null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        try {
+                            JSONObject jsonObject = response.getJSONObject(0);
+                            idUser = jsonObject.getInt("id");
+                            edttenkhachhang.setText(jsonObject.getString("name"));
+                            edtaddress.setText(jsonObject.getString("diaChi"));
+                            edtsdt.setText(String.valueOf(jsonObject.getInt("soDienThoai")));
+                            edtemail.setText(jsonObject.getString("email"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+        );
+        requestQueue.add(jsonArrayRequest);
+    }
     private void EventButton() {
         btnxacnhan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final String ten = edttenkhachhang.getText().toString().trim();
+                final String diachi = edtaddress.getText().toString().trim();
                 final String sdt = edtsdt.getText().toString().trim();
                 final String email = edtemail.getText().toString().trim();
                 if(ten.length()>0 && sdt.length()>0 && email.length()>0){
@@ -128,6 +164,7 @@ public class Thongtinkhachhang extends AppCompatActivity {
                             hashMap.put("tenKhachHang",ten);
                             hashMap.put("soDienThoai",sdt);
                             hashMap.put("email",email);
+                            hashMap.put("diaChi",diachi);
                             return hashMap;
                         }
                     };
@@ -143,6 +180,7 @@ public class Thongtinkhachhang extends AppCompatActivity {
         edttenkhachhang = findViewById(R.id.edittexttenkhachang);
         edtemail = findViewById(R.id.edittextemail);
         edtsdt = findViewById(R.id.edittextsodienthoai);
+        edtaddress = findViewById(R.id.edittextaddress);
         btnxacnhan = findViewById(R.id.buttonxacnhan);
         btntrove = findViewById(R.id.buttontrove);
     }
